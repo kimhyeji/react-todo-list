b341a48309400c900f9463b1d7a5a9843b599ac0
kimhyeji <gpwl5589@gmail.com>
2023-02-09 21:45:56 +0900

    페이지 분리 및 초기 UI 작업 02

--- src/App.js
+++ src/App.js
@@ -8,27 +8,36 @@ import {
 } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
-import Sub1Page from "./pages/Sub1Page";
+import WritePage from "./pages/WritePage";
+import NoticeSnackbar from "./components/NoticeSnackbar";
 
 function App() {
   const location = useLocation();
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
-          <span className="font-bold">앱 이름</span>
+          <span className="font-bold select-none">앱 이름</span>
           <div className="flex-1 flex justify-end">
-            {location.pathname != "/sub1" && (
-              <NavLink to="/sub1">서브1</NavLink>
+            {location.pathname != "/write" && (
+              <NavLink to="/write" className="select-none">
+                글쓰기
+              </NavLink>
             )}
-            {location.pathname == "/sub1" && <NavLink to="/main">이전</NavLink>}
+            {location.pathname == "/write" && (
+              <NavLink to="/main" className="select-none">
+                이전
+              </NavLink>
+            )}
           </div>
         </Toolbar>
       </AppBar>
+      <NoticeSnackbar />
       <Routes>
         <Route path="/main" element={<MainPage />} />
-        <Route path="/sub1" element={<Sub1Page />} />
+        <Route path="/write" element={<WritePage />} />
         <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
     </>
