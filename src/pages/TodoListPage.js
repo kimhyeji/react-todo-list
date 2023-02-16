c687a32310481d1204be9fb0373d2c7afaa5788c
kimhyeji <gpwl5589@gmail.com>
2023-02-16 20:07:28 +0900

    나머지 정렬 기능들 구현

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -32,11 +32,29 @@ export default function TodoList() {
 
   const filteredTodos = getFliteredTodos();
 
-  const sortedTodos = [...filteredTodos].sort((a, b) => {
-    if (a.performDate == b.performDate) return 0;
+  const getSortedTodos = () => {
+    if (sortIndex == 0) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
 
-    return a.performDate > b.performDate ? 1 : -1;
-  });
+        return a.performDate > b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 1) {
+      return [...filteredTodos].sort((a, b) => {
+        if (a.performDate == b.performDate) return 0;
+
+        return a.performDate < b.performDate ? 1 : -1;
+      });
+    } else if (sortIndex == 2) {
+      return [...filteredTodos].sort((a, b) => {
+        return a.id > b.id ? 1 : -1;
+      });
+    }
+
+    return filteredTodos;
+  };
+
+  const sortedTodos = getSortedTodos();
 
   return (
     <>
