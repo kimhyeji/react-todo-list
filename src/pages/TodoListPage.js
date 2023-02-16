2821109f4931a45e8f990066236f73b5575c83bb
kimhyeji <gpwl5589@gmail.com>
2023-02-16 19:45:43 +0900

    일정이 급한 순으로 정렬

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -32,6 +32,12 @@ export default function TodoList() {
 
   const filteredTodos = getFliteredTodos();
 
+  const sortedTodos = [...filteredTodos].sort((a, b) => {
+    if (a.performDate == b.performDate) return 0;
+
+    return a.performDate > b.performDate ? 1 : -1;
+  });
+
   return (
     <>
       <TodoOptionDrawer status={todoOptionDrawerStatus} />
@@ -125,7 +131,7 @@ export default function TodoList() {
 
       <div className="mt-4 px-4">
         <ul>
-          {filteredTodos.map((todo, index) => (
+          {sortedTodos.map((todo, index) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
